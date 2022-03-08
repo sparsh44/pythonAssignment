c38b4083ba80ee621c45707bcac0e7e3097a0e65
sparsh44 <sparshsinghbhatia339@gmail.com>
2022-03-08 14:38:42 +0530

    Python Assignment 5

---
+++ pythonAsg5/Solutions.py
@@ -0,0 +1,20 @@
+#Ques 4
+
+Enter The Numbers:
+23 12 33 34
+List Entered By User: [23, 12, 33, 34]
+Sorted List Using Merge Sort: [12, 23, 33, 34]
+Sorted List Using Quick Sort: [12, 23, 33, 34]
+
+#Ques 5
+Enter The Numbers:
+4 5 2 4 5 6 2 3 4 5 7
+List Entered By User: [4, 5, 2, 4, 5, 6, 2, 3, 4, 5, 7]
+Part a
+Sorted List: [2, 2, 3, 4, 4, 4, 5, 5, 5, 6, 7]
+Enter The Number You Want To Check:
+4 
+Part b
+If the Number is Present: True
+Part c
+Number of times the number entered by user appeared: 3
